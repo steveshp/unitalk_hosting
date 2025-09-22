35365ab60d9448afc96fe9e604e0c0a6bc76761d
Seonghun Park <steveshp@naver.com>
2025-09-22 16:31:31 +0900

    Fix: Production 환경에서도 자동 로그인 활성화 및 디버깅 로그 추가

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "979bc512aa7e7c511e01525d18a9d389",
+const RESOURCES = {"flutter_bootstrap.js": "dc3a03531027fabc18a7fa28002f5119",
 "version.json": "a695952c878feefc321c9008288f3c07",
 "index.html": "0d93b28832a72d2a1eff633448ef703c",
 "/": "0d93b28832a72d2a1eff633448ef703c",
-"main.dart.js": "d586297682ba1a1bcc2c7f5688ef9d72",
+"main.dart.js": "30a19ae594353ed87b48aeeb88aa09d2",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
